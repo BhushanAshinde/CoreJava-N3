59edd4fc0b9f690401bba825046635ab0db2330b
BhushanAshinde <92881526+BhushanAshinde@users.noreply.github.com>
2023-02-02 13:21:30 +0530

    Second commit

---
+++ dayone/src/org/tnsif/intro/DataTypeDemo.java
@@ -0,0 +1,36 @@
+package org.tnsif.intro;
+
+public class DataTypeDemo {
+	public static void main(String[] args) {
+		//byte type
+		byte a=127;
+		//integer type
+		int b=128;
+		//float 
+		float c=5/2.5f;
+		float d=12/5;
+		
+		double e=12/5;
+		double f=12d/5d;
+		
+		//character
+		char z='s';
+		
+		//ASCII representation
+		int x='c';
+		
+		String name="Bhushan";
+		
+				
+		System.out.println("The value of a is: "+a);
+		System.out.println("The value of b is: "+b);
+		System.out.println("The value of c is: "+c);
+		System.out.println("The value of d is: "+d);
+		System.out.println("The value of e is: "+e);
+		System.out.println("The value of e is: "+f);
+		System.out.println("The value of e is: "+z);
+		System.out.println("The value of e is: "+x);
+		System.out.println("The value of e is: "+name);
+	}
+
+}
