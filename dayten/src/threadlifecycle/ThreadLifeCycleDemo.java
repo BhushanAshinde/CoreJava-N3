26523ffa5f986a4cc566f0a59f51051be5374806
BhushanAshinde <92881526+BhushanAshinde@users.noreply.github.com>
2023-02-20 11:48:06 +0530

    first commit

---
+++ dayten/src/threadlifecycle/ThreadLifeCycleDemo.java
@@ -0,0 +1,21 @@
+package threadlifecycle;
+
+
+//driver class
+//program to demonstrate on Thread lifecycle
+public class ThreadLifeCycleDemo {
+
+	public static void main(String[] args) {
+		Tech t=new Tech();
+		SoftSkill s=new SoftSkill();
+		t.start();
+		
+		// Move control to another thread
+		
+		t.yield();
+		
+		s.start();
+
+	}
+
+}
